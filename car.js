0b5f150dda8171eda0b30fcaebedf321bb6895c2
Daniel Gustaw <gustaw.daniel@gmail.com>
2024-10-27 15:21:34 +0400

    nn finished

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-    constructor(x,y,width,height, controlType, maxSpeed = 1) {
+    constructor(x, y, width, height, controlType, maxSpeed = 1) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -28,11 +28,10 @@ class Car {
     update(roadBorders, traffic) {
         if (!this.damaged) {
             this.#move();
-            this.polygon = this.#createPolygon();
+            this.polygon = this.#cratePolygon();
             this.damaged = this.#assessDamage(roadBorders, traffic)
         }
-
-        if(this.sensor) {
+        if (this.sensor) {
             this.sensor.update(roadBorders, traffic);
             const offsets = this.sensor.readings.map(s => s === null ? 0 : 1-s.offset);
             const outputs = NeuralNetwork.feedForward(offsets, this.brain);
@@ -60,64 +59,64 @@ class Car {
         return false;
     }
 
-
-    #createPolygon() {
+    #cratePolygon() {
         const points = [];
         const rad = Math.hypot(this.width, this.height) / 2;
         const alpha = Math.atan2(this.width, this.height);
-
         points.push({
             x: this.x + Math.sin(this.angle - alpha) * rad,
-            y: this.y + Math.cos(this.angle- alpha) * rad
+            y: this.y + Math.cos(this.angle - alpha) * rad,
         });
         points.push({
             x: this.x + Math.sin(this.angle + alpha) * rad,
-            y: this.y + Math.cos(this.angle+ alpha) * rad
+            y: this.y + Math.cos(this.angle + alpha) * rad,
         });
         points.push({
-            x: this.x + Math.sin(Math.PI +this.angle - alpha) * rad,
-            y: this.y + Math.cos(Math.PI +this.angle- alpha) * rad
+            x: this.x + Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y + Math.cos(Math.PI + this.angle - alpha) * rad,
         });
         points.push({
             x: this.x + Math.sin(Math.PI + this.angle + alpha) * rad,
-            y: this.y + Math.cos(Math.PI +this.angle+ alpha) * rad
+            y: this.y + Math.cos(Math.PI + this.angle + alpha) * rad,
         });
         return points;
     }
 
     #move() {
-        if(this.controlls.forward) {
+        if (this.controlls.forward) {
             this.speed += this.acceleration;
         }
-        if(this.controlls.reverse){
+        if (this.controlls.reverse) {
             this.speed -= this.acceleration;
         }
-        if(this.speed > this.maxSpeed) {
+
+        if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
         }
-        if(this.speed < -this.maxSpeed / 2) {
+        if (this.speed < -this.maxSpeed / 2) {
             this.speed = -this.maxSpeed / 2;
         }
 
-        if(this.speed > 0) {
+        if (this.speed > 0) {
             this.speed -= this.friction;
         }
-        if(this.speed < 0) {
+        if (this.speed < 0) {
             this.speed += this.friction;
         }
-        if(Math.abs(this.speed) < this.friction) {
+        if (Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
 
-        if(this.speed !== 0) {
+        if (this.speed !== 0) {
             const flip = this.speed > 0 ? 1 : -1;
-            if(this.controlls.left) {
+            if (this.controlls.left) {
                 this.angle += 0.03 * flip;
             }
-            if(this.controlls.right) {
+            if (this.controlls.right) {
                 this.angle -= 0.03 * flip;
             }
         }
+
 
         this.x -= Math.sin(this.angle) * this.speed;
         this.y -= Math.cos(this.angle) * this.speed;
@@ -133,9 +132,9 @@ class Car {
         ctx.beginPath();
         ctx.lineWidth = 0;
         ctx.strokeStyle = 'transparent';
-        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
         for (let i = 1; i < this.polygon.length; i++) {
-            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
         }
         ctx.fill();
         ctx.stroke();
--- controls.js
+++ controls.js
@@ -1,8 +1,8 @@
 class Controls {
     constructor(type) {
         this.forward = false;
-        this.right = false;
         this.left = false;
+        this.right = false;
         this.reverse = false;
 
         switch (type) {
@@ -16,38 +16,38 @@ class Controls {
     }
 
     #addKeyboardListener() {
-        document.addEventListener('keydown', (e) => {
-            switch(e.key) {
-                case 'ArrowUp':
-                    this.forward = true;
-                    break;
+        document.onkeydown = (event) => {
+            switch (event.key) {
                 case 'ArrowLeft':
                     this.left = true;
                     break;
                 case 'ArrowRight':
                     this.right = true;
                     break;
+                case 'ArrowUp':
+                    this.forward = true;
+                    break;
                 case 'ArrowDown':
                     this.reverse = true;
                     break;
             }
-        });
+        }
 
-        document.addEventListener('keyup', (e) => {
-            switch(e.key) {
-                case 'ArrowUp':
-                    this.forward = false;
-                    break;
+        document.onkeyup = (event) => {
+            switch (event.key) {
                 case 'ArrowLeft':
                     this.left = false;
                     break;
                 case 'ArrowRight':
                     this.right = false;
                     break;
+                case 'ArrowUp':
+                    this.forward = false;
+                    break;
                 case 'ArrowDown':
                     this.reverse = false;
                     break;
             }
-        });
+        }
     }
 }
